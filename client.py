04739378963275bf73d3ca69c1299164bd8f1286
Srcafe <diegohmarques.ti@gmail.com>
2021-06-18 16:08:31 -0300

    Thread

--- client.py
+++ client.py
@@ -14,12 +14,12 @@ def read_file(file_name):
     with open(file_name,'r',encoding="utf8") as f:
         for line in f:
             for word in line.split():
-                try:
+#                try:
 
                     client_x.put(word,5)
                     print("Adicionando " + word)
-                except:
-                    print(word + " ja existe")
+#                except:
+#                    print(word + " ja existe")
 
 
 def search_word(file_name):
@@ -38,26 +38,26 @@ def search_word(file_name):
 
                 
 reader_one = Thread(target=read_file,args=['txt-biblia.txt'])
-reader_two = Thread(target=read_file,args=['txt-hp1.txt'])
-reader_three = Thread(target=read_file,args=['txt-hp2.txt'])
-reader_four = Thread(target=read_file,args=['txt-hp3.txt'])
-reader_five = Thread(target=read_file,args=['txt-hp4.txt'])
+#reader_two = Thread(target=read_file,args=['txt-hp1.txt'])
+#reader_three = Thread(target=read_file,args=['txt-hp2.txt'])
+#reader_four = Thread(target=read_file,args=['txt-hp3.txt'])
+#reader_five = Thread(target=read_file,args=['txt-hp4.txt'])
 
-searcher_one = Thread(target=search_word,args=['txt-hp1.txt'])
-searcher_two = Thread(target=search_word,args=['txt-hp2.txt'])
-searcher_three = Thread(target=search_word,args=['txt-hp3.txt'])
-searcher_four = Thread(target=search_word,args=['txt-hp4.txt'])
+#searcher_one = Thread(target=search_word,args=['txt-hp1.txt'])
+#searcher_two = Thread(target=search_word,args=['txt-hp2.txt'])
+#searcher_three = Thread(target=search_word,args=['txt-hp3.txt'])
+#searcher_four = Thread(target=search_word,args=['txt-hp4.txt'])
 
 
 reader_one.start()
-reader_two.start()
-reader_three.start()
-reader_four.start()
-reader_five.start()
+#reader_two.start()
+#reader_three.start()
+#reader_four.start()
+#reader_five.start()
 
-searcher_one.start()
-searcher_two.start()
-searcher_three.start()
-searcher_four.start()
+#searcher_one.start()
+#searcher_two.start()
+#searcher_three.start()
+#searcher_four.start()
 
 client_x.destroy()
